8a9b59ba86b2a94cd75b74c60c233f5c6d301b0f
luizfeliperamossouza <44178898+luizfeliperamossouza@users.noreply.github.com>
2020-11-29 14:15:53 -0300

    Add files via upload

---
+++ rastreio/Cod.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Rastreio</title>
+		<link rel="stylesheet" type="text/css" href="css/stylle.css">
+    </head>
+    <body>
+	
+	<form class="login-form" action="rastrear.php">
+		<h1>Rastrear Objeto</h1>
+		<div class="form-input-material">
+		<label for="username">Digite o código</label>
+		<input type="text" name="username" id="username" placeholder=" " autocomplete="off" class="form-control-material" required />
+		</div>
+		<button type="submit" class="btn btn-primary btn-ghost">Rastrear</button>
+</form>
+	
+    </body>
+</html>
+
